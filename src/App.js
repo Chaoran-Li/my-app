79c5c7b5dd1cafc2f7fba39473a0b9d3ad2d8926
Sean Li <lichaoran520@gmail.com>
2020-02-23 18:29:47 +1000

    add search box component and rewrite the handle change method

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 
 class App extends Component {
@@ -19,18 +20,23 @@ class App extends Component {
     .then(users => this.setState({ monster: users }));
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value});
+  }
+
   render() {
+    const { monster, searchField } = this.state;
+    const filteredMonsters = monster.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className='App'>
-      <input 
-        type='search' 
-        placeholder='search monsters' 
-        onChange={e => {
-          this.setState({ searchField: e.target.value });
-          }
-        } 
+      <SearchBox 
+        placeholder='search monsters'
+        handleChange={ this.handleChange }
       />
-      <CardList monster={ this.state.monster } />
+      <CardList monster={ filteredMonsters } />
       </div>
     );
   }
